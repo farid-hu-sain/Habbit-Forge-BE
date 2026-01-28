3a4222c7104741a3b22abad205f33c9f8c90f84a
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-01-28 09:38:36 +0700

    feat: fix app

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,6 @@ import swaggerSpec from "./utils/swagger.js";
 import { successResponse } from "./utils/response.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-import categoryRoutes from "./routes/category.route.js";
 import habitRoutes from "./routes/habit.route.js";
 import checkInRoutes from "./routes/checkIn.route.js";
 import profileRoutes from "./routes/profile.route.js";
@@ -65,7 +64,6 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-app.use("/api/category", categoryRoutes);
 app.use("/api/habit", habitRoutes);
 app.use("/api/checkIn", checkInRoutes);
 app.use("/api/profile", profileRoutes);
